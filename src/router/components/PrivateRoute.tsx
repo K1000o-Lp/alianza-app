9ef18cbbc086b31c018bd332c127000ed8ce8304
k1000o-lp <camiloalfonsolopezparra@gmail.com>
2025-01-28 09:42:47 -0400

    Refactor PrivateRoute and PublicRoute components to include loading state and session verification with a loading banner

--- src/router/components/PrivateRoute.tsx
+++ src/router/components/PrivateRoute.tsx
@@ -1,9 +1,11 @@
-import { FC, ReactNode, useEffect } from "react";
+import { FC, ReactNode, useEffect, useState } from "react";
 import { RootState, useAppDispatch, useAppSelector } from "../../redux/store";
 import { Navigate } from "react-router-dom";
 import { jwtDecode } from "jwt-decode";
 import { setUser } from "../../redux/features/authSlice";
 import { User } from "../../types";
+
+import bannerDarkUrl from "/banner-dark.webp";
 
 interface Props {
   children: ReactNode;
@@ -11,20 +13,34 @@ interface Props {
 
 export const PrivateRoute: FC<Props> = ({ children }) => {
   const dispatch = useAppDispatch();
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem("token");
   const auth = useAppSelector((state: RootState) => state.auth);
-  
-  useEffect(() => {
-    if(!auth.user && token) {
-      const { session }: {session: User} = jwtDecode(token);
-      dispatch(setUser(session));
-    }
-  }, [auth.user])
 
-  if(!token) {
-    return <Navigate to="auth/signin"/>
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const verifySession = async () => {
+      if (!auth.user && token) {
+        try {
+          const { session }: { session: User } = jwtDecode(token);
+          dispatch(setUser(session));
+        } catch (error) {
+          console.error("Error decoding token:", error);
+        }
+      }
+      setLoading(false);
+    };
+
+    verifySession();
+  }, [auth.user, token, dispatch]);
+
+  if (loading) {
+    return <div style={{height: '100vh', width: '100vw', display: 'flex', justifyContent: 'center', alignItems: 'center'}}><img src={bannerDarkUrl} alt="logo" style={{ width: "250px"}} /></div>;
   }
 
+  if (!token) {
+    return <Navigate to="/auth/signin" />;
+  }
 
-  return auth.isLogged ? children : <Navigate to="auth/signin" replace />;
+  return auth.isLogged ? children : <Navigate to="/auth/signin" replace />;
 };
--- src/router/components/PublicRoute.tsx
+++ src/router/components/PublicRoute.tsx
@@ -1,14 +1,42 @@
-import { FC, ReactNode } from "react";
-import { RootState, useAppSelector } from "../../redux/store";
+import { FC, ReactNode, useEffect, useState } from "react";
+import { RootState, useAppDispatch, useAppSelector } from "../../redux/store";
 import { Navigate } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
+import { User } from "../../types";
+import { setUser } from "../../redux/features/authSlice";
+
+import bannerDarkUrl from "/banner-dark.webp";
 
 interface Props {
   children: ReactNode;
 }
 
 export const PublicRoute: FC<Props> = ({ children }) => {
+  const dispatch = useAppDispatch();
   const token = localStorage.getItem('token');
   const auth = useAppSelector((state: RootState) => state.auth);
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const verifySession = async () => {
+      if (!auth.user && token) {
+        try {
+          const { session }: { session: User } = jwtDecode(token);
+          dispatch(setUser(session));
+        } catch (error) {
+          console.error("Error decoding token:", error);
+        }
+      }
+      setLoading(false);
+    };
+
+    verifySession();
+  }, [auth.user, token, dispatch]);
+
+  if (loading) {
+    return <div style={{height: '100vh', width: '100vw', display: 'flex', justifyContent: 'center', alignItems: 'center'}}><img src={bannerDarkUrl} alt="logo" style={{ width: "250px"}} /></div>;
+  }
 
   if(token) {
     return <Navigate to="/" />
